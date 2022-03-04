d406b8bf06c54abec5f9c9d27f92067f47fbee9e
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-03-04 23:14:40 +0530

    Time: 37 ms (87.80%), Space: 21.2 MB (63.64%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     string getSmallestString(int n, int k) {
-        string str="";
-        int i=26;
-        while(n>1){
-            while(double((k-i)/(n-1))<1) i--;
-            str+=char(96+i);
-            n--, k-=i;
+        string ans=string(n, 'a');
+        k-=n;
+        while(k>0){
+            ans[--n]+=min(25, k);
+            k-=min(25, k);
         }
-        str+=char(96+k);
-        reverse(str.begin(), str.end());
-        return str;
+        return ans;
     }
 };

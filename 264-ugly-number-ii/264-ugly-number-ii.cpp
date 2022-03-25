7119026694df7eff558ec3c967164369b8dedc23
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-03-25 22:57:20 +0530

    Time: 23 ms (43.17%), Space: 7.7 MB (67.86%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<long long> dp(n+1, 0);
-        dp[1]=1;
-        for(int i=2;i<n+1;i++){
-            long long mn=LLONG_MAX;
-            bool f1=false, f2=false, f3=false;
-            for(int j=1;j<i;j++){
-                if(dp[j]*2>dp[i-1]){
-                    mn=min(mn, dp[j]*2);
-                    f1=true;
-                }
-                if(dp[j]*3>dp[i-1]){
-                    mn=min(mn, dp[j]*3);
-                    f2=true;
-                }
-                if(dp[j]*5>dp[i-1]){
-                    mn=min(mn, dp[j]*5);
-                    f3=true;
-                }
-                if(f1&&f2&&f3) break;
-            }
-            dp[i]=mn;
+        vector<int> dp(n, 1);
+        int l2=0, l3=0, l5=0;
+        for(int i=1;i<n;i++){
+            dp[i]=min(dp[l2]*2, min(dp[l3]*3, dp[l5]*5));
+            if(dp[i]==dp[l2]*2) l2++;
+            else if(dp[i]==dp[l3]*3) l3++;
+            else if(dp[i]==dp[l5]*5) l5++;
+            // cout<<i<<" "<<dp[i]<<" "<<l2<<" "<<l3<<" "<<l5<<endl;
+            if(dp[i]<=dp[i-1]) i--;
         }
-        return dp[n];
+        // for(int i=0;i<n;i++) cout<<dp[i]<<" ";
+        return dp[n-1];
     }
 };

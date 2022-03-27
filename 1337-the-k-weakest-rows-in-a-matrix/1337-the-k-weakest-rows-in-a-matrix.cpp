7dcf3b5f76ef5e695ce789a925458765777e0c4f
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-03-27 12:26:12 +0530

    Time: 17 ms (50.87%), Space: 10.5 MB (42.57%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,12 +1,17 @@
 #define pii pair<int, int>
 class Solution {
 public:
-    // static comp(pair<int, int> p1, pair<int, int> p2){
-    //     if(p1.first!=p2.first) return p1.first>p2.first;
-    //     else p1.second<p2.second;
-    // }
+    class comp
+{
+public:
+    bool operator() (pair<int, int> p1, pair<int, int> p2){
+        if(p1.first!=p2.first) return p1.first<p2.first;
+        else return p1.second<p2.second;
+    }
+};
+    
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        priority_queue<pii> pq;
+        priority_queue<pii, vector<pii>, comp> pq;
         int m=mat.size(), n=mat[0].size();
         vector<int> ones(m);
         for(int i=0;i<m;i++){

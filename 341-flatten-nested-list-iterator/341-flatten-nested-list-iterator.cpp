02056290bf3e83d9a9b6f1e8f044fa99be8ee64d
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-05-08 19:39:30 +0530

    Time: 26 ms (27.30%), Space: 14.2 MB (45.15%) - LeetHub

---
+++ 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.cpp
@@ -0,0 +1,45 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class NestedIterator {
+queue<int> data;
+    
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        flatten(nestedList);
+    }
+    
+    void flatten(vector<NestedInteger> &list) {
+        for (NestedInteger el : list)
+            if (el.isInteger()) data.push(el.getInteger());
+            else flatten(el.getList());
+    }
+    
+    int next() {
+        int res = data.front(); data.pop();
+        return res;
+    }
+    
+    bool hasNext() { return data.size() > 0; }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */

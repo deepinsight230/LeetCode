8026bbdf05381764372115521e602584167183a7
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-05-16 02:44:29 +0530

    Time: 24 ms (5.22%), Space: 9.5 MB (87.69%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for(int i: nums){
-            mp[i]++;
+        int n=nums.size();
+        long long ans=0;
+        for(int i=0;i<32;i++){
+            int cnt=0;
+            for(int j=0;j<n;j++){
+                if((abs(nums[j])&(1<<i))!=0) cnt++;
+            }
+            if(cnt%3!=0) ans=ans+(pow(2, i));
         }
-        for(auto m: mp){
-            if(m.second==1) return m.first;
+        int negans=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==-ans) negans++;
         }
-        return 0;
+        return (negans==1)?-ans:ans;
     }
 };

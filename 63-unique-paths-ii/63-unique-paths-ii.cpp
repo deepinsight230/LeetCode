26f4d2f8a71069cb221c1cf4c02f0648e81359c1
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-05-20 16:34:06 +0530

    Time: 22 ms (5.09%), Space: 7.9 MB (41.40%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -2,20 +2,28 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m=grid.size(), n=grid[0].size();
-        vector<vector<long long>> dp(m, vector<long long> (n, 0));
-        dp[m-1][n-1]=1-grid[m-1][n-1];
-        for(int i= m-2;i>=0;i--)
-            if(grid[i][n-1]==1) dp[i][n-1]=0;
-            else dp[i][n-1]=dp[i+1][n-1];
-        for(int i=n-2;i>=0;i--)
-            if(grid[m-1][i]==1) dp[m-1][i]=0;
-            else dp[m-1][i]=dp[m-1][i+1];
-        for(int i=m-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                if(grid[i][j]==1) dp[i][j]=0;
-                else dp[i][j]=dp[i+1][j]+dp[i][j+1];
-            }
-        }
-        return (int)dp[0][0];
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+        dp[0][0]=1-grid[0][0];
+        for(int i=1;i<m;i++)
+            if(grid[i][0]==0&&dp[i-1][0]!=0)
+                dp[i][0]=dp[i-1][0];
+        for(int i=1;i<n;i++)
+            if(grid[0][i]==0&&dp[0][i-1]!=0)
+                dp[0][i]=dp[0][i-1];
+         for(int i=1;i<m;i++){
+             for(int j=1;j<n;j++){
+                 if(grid[i][j]==0){
+                     dp[i][j]+=(dp[i-1][j]==0)?0:dp[i-1][j];
+                     dp[i][j]+=(dp[i][j-1]==0)?0:dp[i][j-1];
+                 }
+             }
+         }
+         for(int i=0;i<m;i++){
+             for(int j=0;j<n;j++){
+                 cout<<dp[i][j]<<" ";
+             }
+             cout<<endl;
+         }
+         return dp[m-1][n-1];
     }
 };

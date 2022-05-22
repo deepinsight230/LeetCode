922a6e5eb7e6b287d3c9c3b1c797e36b8c00a3e2
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-05-23 01:03:20 +0530

    Time: 1515 ms (5.01%), Space: 217.7 MB (14.31%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int helper(int day, bool bought, int k, vector<int> &prices, vector<vector<vector<int>>> &dp){
+        if(k<=-1||day>=prices.size()) return 0;
+        if(dp[day][bought][k]!=-1) return dp[day][bought][k];
+        if(bought==false){
+            int buy=-prices[day]+helper(day+1, true, k, prices, dp);
+            int notbuy=helper(day+1, false, k, prices, dp);
+            return dp[day][bought][k]=max(buy, notbuy);
+        }
+        else{
+            int sell=prices[day]+helper(day+1, false, k-1, prices, dp);
+            int notsell=helper(day+1, true, k, prices, dp);
+            return dp[day][bought][k]=max(sell, notsell);
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        // cout<<n;
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(2, -1)));
+        return helper(0, false, 1, prices, dp);
+    }
+};

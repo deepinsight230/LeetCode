4ef9f301dd4001437adaa3cf63b82cbe78a9cd8b
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-05-25 02:59:27 +0530

    Time: 24 ms (13.00%), Space: 12 MB (21.46%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int helper(int day, bool bought, vector<int> &prices, vector<vector<int>> &dp){
-        if(day>=prices.size()) return 0;
-        if(dp[day][bought]!=-1) return dp[day][bought];
+    int helper(int i, bool bought, vector<int> &prices, vector<vector<int>> &dp){
+        if(i>=prices.size()) return 0;
+        if(dp[i][bought]!=-1) return dp[i][bought];
         if(bought==false){
-            int buy=-prices[day]+helper(day+1, true, prices, dp);
-            int notbuy=helper(day+1, false, prices, dp);
-            return dp[day][bought]=max(buy, notbuy);
+            int buy=-prices[i]+helper(i+1, true, prices, dp);
+            int notbuy=helper(i+1, false, prices, dp);
+            return dp[i][bought]=max(buy, notbuy);
         }
         else{
-            int sell=prices[day]+helper(day+2, false, prices, dp);
-            int notsell=helper(day+1, true, prices, dp);
-            return dp[day][bought]=max(sell, notsell);
+            int sell=prices[i]+helper(i+2, false, prices, dp);
+            int notsell=helper(i+1, true, prices, dp);
+            return dp[i][bought]=max(sell, notsell);
         }
     }
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>> dp(n, vector<int> (2, -1));
+        vector<vector<int>> dp(prices.size(), vector<int> (2, -1));
         return helper(0, false, prices, dp);
     }
 };

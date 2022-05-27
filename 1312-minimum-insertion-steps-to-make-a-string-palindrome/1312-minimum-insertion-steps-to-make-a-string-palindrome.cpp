02535a93fa8dffc00fa94d8c2383f5866795d12c
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-05-28 02:57:12 +0530

    Time: 118 ms (13.34%), Space: 27.2 MB (24.46%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int lcs(string s, string t){
-        int n=s.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(s[i-1]==t[j-1])
-                    dp[i][j]=1+dp[i-1][j-1];
-                else
-                    dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
-            }
+    int lcs(int m, int n, string &s, string &t, vector<vector<int>> &dp){
+        if(m<0) return 0;
+        if(n<0) return 0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        if(s[m]==t[n]){
+            return dp[m][n]=1+lcs(m-1, n-1, s, t, dp);
         }
-        return dp[n][n];
+        else{
+            return dp[m][n]=max(lcs(m-1, n, s, t, dp), lcs(m, n-1, s, t, dp));
+        }
     }
     int minInsertions(string s) {
         string t=s;
         reverse(t.begin(), t.end());
         int n=s.size();
-        int lcp=lcs(s, t);
+        // cout<<n;
+        vector<vector<int>> dp(n, vector<int> (n, -1));
+        int lcp=lcs(n-1, n-1, s, t, dp);
         return (n-lcp);
     }
 };

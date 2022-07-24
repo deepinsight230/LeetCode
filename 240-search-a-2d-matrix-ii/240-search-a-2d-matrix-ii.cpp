b31f6e7f51a4f77614406d13ba842b3fae596cef
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-07-24 21:41:47 +0530

    Time: 195 ms (40.95%), Space: 15 MB (21.86%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -5,7 +5,7 @@ public:
         int i=0, j=n-1;
         while(i<m&&j>=0){
             if(matrix[i][j]==target) return true;
-            if(matrix[i][j]>target) j--;
+            else if(matrix[i][j]>target) j--;
             else i++;
         }
         return false;

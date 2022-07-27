d52b24646e9fbacab135096c628af36998f05def
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-07-27 05:52:26 +0530

    Time: 7 ms (87.13%), Space: 14.8 MB (81.00%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,26 +10,33 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(!l1) return l2;
-        if(!l2) return l1;
-        ListNode *head, *curr;
-        if(l1->val<l2->val){
-            head=l1;
-            l1=l1->next;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *dummy=new ListNode(0);
+        ListNode *dhead=dummy;
+        ListNode *curr1=list1, *curr2=list2;
+        while(curr1 && curr2){
+            if(curr1->val<=curr2->val){
+                dhead->next=curr1;
+                curr1=curr1->next;
+            }
+            else{
+                dhead->next=curr2;
+                curr2=curr2->next;
+            }
+            dhead=dhead->next;
         }
-        else{
-            head=l2;
-            l2=l2->next;
+        while(curr1){
+            dhead->next=curr1;
+            curr1=curr1->next;
+            dhead=dhead->next;
         }
-        curr=head;
-        while(l1&&l2){
-            if(l1->val<l2->val) curr->next=l1,  l1=l1->next;
-            else curr->next=l2, l2=l2->next;
-            curr=curr->next;
+        while(curr2){
+            dhead->next=curr2;
+            curr2=curr2->next;
+            dhead=dhead->next;
         }
-        if(l1) curr->next=l1;
-        if(l2) curr->next=l2;
-        return head;
+        ListNode *dnext=dummy->next;
+        delete dummy;
+        return dnext;
     }
 };

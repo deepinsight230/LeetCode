07d7e2afa0f16bd0a390d820180088dc8469082c
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-08-05 04:38:59 +0530

    Time: 44 ms (15.89%), Space: 7 MB (51.45%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -2,29 +2,23 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         unordered_map<char, int> mp;
-        int i=0, j=0, n=s.size(), ans=0;
+        int i=0, j=0, n=s.size();
+        int ans=0;
         while(j<n){
             mp[s[j]]++;
-            int winlen=j-i+1;
-            int mfreq=0;
-            for(auto m: mp){
-                mfreq=max(mfreq, m.second);
-            }
-            int extras=winlen-mfreq;
-            if(extras<=k){
-                ans=max(ans, winlen);
+            int maxfreq=0;
+            for(auto m: mp) maxfreq=max(maxfreq, m.second);
+            int winLen=j-i+1;
+            if(winLen-maxfreq <=k){
+                ans=max(ans, winLen);
                 j++;
             }
             else{
-                while(extras>k){
+                while(winLen-maxfreq>k){
                     mp[s[i]]--;
                     i++;
-                    winlen=j-i+1;
-                    mfreq=0;
-                    for(auto m: mp){
-                        mfreq=max(mfreq, m.second);
-                    }
-                    extras=winlen-mfreq;
+                    for(auto m: mp) maxfreq=max(maxfreq, m.second);
+                    winLen=j-i+1;
                 }
                 j++;
             }

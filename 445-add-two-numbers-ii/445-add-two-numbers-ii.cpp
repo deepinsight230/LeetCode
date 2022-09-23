7f823855488e34fddd73559da051f1f1d8bf7234
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2022-09-23 13:44:56 +0530

    Time: 67 ms (42.53%), Space: 71.3 MB (46.62%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -10,7 +10,7 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode *head){
+    ListNode* reverseList(ListNode *head){
         ListNode *curr=head, *prev=NULL;
         while(curr){
             ListNode *next=curr->next;
@@ -20,45 +20,53 @@ public:
         }
         return prev;
     }
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        l1=reverse(l1);
-        l2=reverse(l2);
-        int carry=0, plus, sum;
-        ListNode *curr1=l1, *curr2=l2, *ret=new ListNode(), *curr=ret;
-        while(curr1&&curr2){
-            sum=curr1->val+curr2->val+carry;
-            plus=sum%10;
-            carry=sum/10;
-            ListNode *node=new ListNode(plus);
-            curr->next=node;
-            curr=curr->next;
-            curr1=curr1->next;
-            curr2=curr2->next;
-        }
-        while(curr1){
-            sum=curr1->val+carry;
-            plus=sum%10;
-            carry=sum/10;
-            ListNode *node=new ListNode(plus);
-            curr->next=node;
-            curr=curr->next;
-            curr1=curr1->next;
-        }
-        while(curr2){
-            sum=curr2->val+carry;
-            plus=sum%10;
-            carry=sum/10;
-            ListNode *node=new ListNode(plus);
-            curr->next=node;
-            curr=curr->next;
-            curr2=curr2->next;
-        }
-        if(carry!=0){
-            ListNode *node=new ListNode(carry);
-            curr->next=node;
-            curr=curr->next;
-        }
-        ret=reverse(ret->next);
-        return ret;
+    ListNode* addTwoNumbers(ListNode* first, ListNode* second) {
+        ListNode *curr1=reverseList(first), *curr2=reverseList(second);
+    ListNode *ans = new  ListNode(0);
+    // ans->data=0;
+    ListNode *anshead=ans;
+    int carry=0, sum=0;
+    while(curr1 && curr2){
+        sum=curr1->val + curr2->val + carry;
+        int rem=sum%10;
+        carry=sum/10;
+        ListNode *node=new ListNode();
+        node->val = rem;
+        anshead->next=node;
+        anshead=anshead->next;
+        curr1=curr1->next;
+        curr2=curr2->next;
+    }
+    while(curr1){
+        sum=curr1->val + carry;
+        int rem=sum%10;
+        carry=sum/10;
+        ListNode *node=new ListNode();
+        node->val = rem;
+        anshead->next=node;
+        anshead=anshead->next;
+        curr1=curr1->next;
+    }
+    while(curr2){
+        sum=curr2->val + carry;
+        int rem=sum%10;
+        carry=sum/10;
+        ListNode *node=new ListNode();
+        node->val = rem;
+        anshead->next=node;
+        anshead=anshead->next;
+        curr2=curr2->next;
+    }
+    if(carry!=0){
+        ListNode *node=new ListNode();
+        node->val = carry;
+        anshead->next=node;
+        anshead=anshead->next;
+    }
+    ListNode *temp=ans;
+    ans=ans->next;
+    delete temp;
+    
+    return reverseList(ans);
     }
 };

87e4ae2bc1caa30c3c607dd2bcbea75157ab5c17
Deep Jha <70419694+deepinsight230@users.noreply.github.com>
2023-01-21 01:21:37 +0530

    Time: 2 ms (80.72%), Space: 7 MB (89.78%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    int ans=0;
-    void subsets(int idx, vector<int> &ds, vector<int> &nums, int n){
+    int subsets(int idx, int xoR, vector<int> &nums, int n){
         if(idx == n){
-            int xr=0;
-            for(int i=0;i<ds.size();i++){
-                xr^=ds[i];
-            }
-            ans+=xr;
-            return;
+            return xoR;
         }
-        ds.push_back(nums[idx]);
-        subsets(idx+1, ds, nums, n);
-        ds.pop_back();
-        subsets(idx+1, ds, nums, n);
+        int inc=subsets(idx+1, xoR^nums[idx], nums, n);
+        int exc=subsets(idx+1, xoR, nums, n);
+        return inc+exc;
     }
     int subsetXORSum(vector<int>& nums) {
         int n=nums.size();
-        vector<int> ds;
-        subsets(0, ds, nums, n);
-        return ans;
+        return subsets(0, 0, nums, n);
     }
 };
